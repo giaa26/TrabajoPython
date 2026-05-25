ae74910995682342c8ce0275e90e98b5cc1f374d
Gianella <danisagianella@gmail.com>
2026-05-25 20:27:10 -0300

    Actualización del proyecto

--- ejercicio1_libros/biblioteca.py
+++ ejercicio1_libros/biblioteca.py
@@ -10,6 +10,28 @@ class Biblioteca:
         self.miembros.append(miembro)
 
     def mostrar_libros(self):
+        print("\nLISTA DE LIBROS")
+
         for libro in self.libros:
-            estado = "Disponible" if libro.disponible else "Prestado"
-            print(f"{libro.titulo} - {estado}")
+
+            if libro.disponible:
+                estado = "Disponible"
+            else:
+                estado = "Prestado"
+
+            print(f"""
+Título: {libro.titulo}
+Autor: {libro.autor}
+ISBN: {libro.isbn}
+Estado: {estado}
+""")
+
+    def mostrar_miembros(self):
+        print("\nLISTA DE MIEMBROS")
+
+        for miembro in self.miembros:
+
+            print(f"""
+Nombre: {miembro.nombre}
+DNI: {miembro.dni}
+""")
--- ejercicio1_libros/main.py
+++ ejercicio1_libros/main.py
@@ -4,19 +4,21 @@ from biblioteca import Biblioteca
 
 biblioteca = Biblioteca()
 
-libro1 = Libro("Python Básico", "Juan Pérez", "123")
-miembro1 = Miembro("Evelyn", "45678912")
+libro1 = Libro("Harry Potter", "J.K Rowling", "111")
+libro2 = Libro("El Principito", "Antoine de Saint-Exupéry", "222")
+
+miembro1 = Miembro("Evelyn", "12345678")
 
 biblioteca.agregar_libro(libro1)
+biblioteca.agregar_libro(libro2)
+
 biblioteca.agregar_miembro(miembro1)
 
 try:
-    miembro1.prestar_libro(libro1)
-    print("Libro prestado correctamente")
-
     miembro1.prestar_libro(libro1)
 
 except Exception as e:
     print("Error:", e)
 
 biblioteca.mostrar_libros()
+biblioteca.mostrar_miembros()
