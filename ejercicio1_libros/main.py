4b90c70861c66f0a1c4055a6ef1fecb43afda9b2
Gianella <danisagianella@gmail.com>
2026-05-25 20:58:11 -0300

    Agregue pruebas y validaciones

--- ejercicio1_libros/main.py
+++ ejercicio1_libros/main.py
@@ -13,12 +13,14 @@ biblioteca.agregar_libro(libro1)
 biblioteca.agregar_libro(libro2)
 
 biblioteca.agregar_miembro(miembro1)
+libro1 = Libro("Harry Potter", "Rowling", "123")
+miembro1 = Miembro("Juan", "001")
 
 try:
     miembro1.prestar_libro(libro1)
+    print("✅ Libro prestado correctamente")
 
 except Exception as e:
     print("Error:", e)
-
 biblioteca.mostrar_libros()
 biblioteca.mostrar_miembros()
